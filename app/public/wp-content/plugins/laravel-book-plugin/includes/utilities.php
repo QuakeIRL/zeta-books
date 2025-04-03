23a4ccd3a88e2b65844e33ee6a340a57e8e02245
Charlotte <77845701+QuakeIRL@users.noreply.github.com>
2025-04-03 17:22:20 -0400

    stubbed data and used it in the template for books

--- app/public/wp-content/plugins/laravel-book-plugin/includes/utilities.php
+++ app/public/wp-content/plugins/laravel-book-plugin/includes/utilities.php
@@ -13,20 +13,80 @@ add_shortcode('api_author_works', 'laravel_author_works_api');
 
 function laravel_book_api()
 {
-    $results = laravel_get_action(LARAVEL_API . '/book');
+    $title = get_the_title();
+    // $results = laravel_get_action(LARAVEL_API . '/book', $title);
+    $results = [];
+    if ($title == 'Dungeon Crawler Carl') {
+        $results = [
+            'title' => 'Dungeon Crawler Carl',
+            'pages' => '464',
+            'publisher' => 'Penguin Publishing Group',
+            'published' => '2024-08-07',
+            'format' => 'hardcover',
+        ];
+    } else {
+        return '<p> No Additional Book data retrieved </p>';
+    }
+
+    $html = '';
+    $html .= '<p>';
+    $html .= '<b>Published by: </b>' . $results['publisher'] . '</br>';
+    $html .= '<b>Publication Date: </b>' . $results['published'] . '</br>';
+    $html .= '<b>Print format: </b>' . ucfirst($results['format']);
+    $html .= '</p>';
+    return $html;
 }
 
 function laravel_author_api()
 {
-    $results = laravel_get_action(LARAVEL_API . '/author');
+    // $results = laravel_get_action(LARAVEL_API . '/author', $author);
+    $results = [
+        'fullname' => 'Matt Dinniman',
+        'website' => 'https://mattdinniman.com',
+        'note' => 'Matt Dinniman rocks'
+    ];
+    return $results;
 }
 
 function laravel_author_works_api()
 {
-    $results = laravel_get_action(LARAVEL_API . '/author/works');
+    // $results = laravel_get_action(LARAVEL_API . '/author/works', $author);
+    $results = [
+        'collected-works' => [
+            [
+                'title' => 'Dungeon Crawler Carl',
+                'pages' => '464',
+                'publisher' => 'Penguin Publishing Group',
+                'published' => '2024-08-07',
+                'format' => 'hardcover',
+            ],
+            [
+                'title' => 'Carl\'s Doomsday Scenario',
+                'pages' => '384',
+                'publisher' => 'Penguin Publishing Group',
+                'published' => '2024-09-24',
+                'format' => 'hardcover',
+            ],
+            [
+                'title' => 'Dungeon Anarchist\'s Cookbook',
+                'pages' => '544',
+                'publisher' => 'Penguin Publishing Group',
+                'published' => '2024-10-22',
+                'format' => 'hardcover',
+            ],
+            [
+                'title' => 'Dominion of Blades',
+                'pages' => '438',
+                'publisher' => 'CreateSpace Independent Publishing Platform',
+                'published' => '2017-03-21',
+                'format' => 'paperback',
+            ],
+        ]
+    ];
+    return $results;
 }
 
-function laravel_get_action( $url)
+function laravel_get_action($url, $stringParam)
 {
     $arguments = array(
         'method' => 'GET',
